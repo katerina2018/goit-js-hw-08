a67d1e94642257d3b096c0fa575015497be64023
katerina2018 <tvorchestvo15@gmail.com>
2021-11-15 12:47:32 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryItemWrapper = document.querySelector('.gallery');
+
+galleryItemWrapper.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                
+                    <a class="gallery__link" href="${original}">
+                        <img 
+                        class="gallery__image"
+                        src="${preview}"                       
+                        alt="${description}"/>
+                    </a>
+                 `;
+        })
+        .join(' ');
+}
