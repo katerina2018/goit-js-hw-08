84afc7dfafec41d610c50afe6bbd0f26ee7c4c68
katerina2018 <tvorchestvo15@gmail.com>
2021-11-24 16:18:12 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const player = new Player('vimeo-player', {
+    width: 640,
+});
+
+player.on(
+    'timeupdate',
+    throttle(function(data) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+    }, 1000),
+);
+window.addEventListener('DOMContentLoaded', () => {
+    let seconds = localStorage.getItem('videoplayer-current-time');
+    player.setCurrentTime(seconds);
+});
